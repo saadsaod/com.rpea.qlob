bd3a1713d793e0757986d6ab7761664e597326fa
saadsaod <talmbktsar@gmail.com>
2026-07-24 06:58:00 +0300

    Disable Google services plugin
    Comment out the Google services plugin in build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
   alias(libs.plugins.google.devtools.ksp)
   alias(libs.plugins.roborazzi)
   alias(libs.plugins.secrets)
-  alias(libs.plugins.google.services)
+// alias(libs.plugins.google.services)
 }
 
 android {
